dadf54caf29bfe74417f69c6ac0744b4863ed619
unknown <vonhathung3012@gmail.com>
2023-07-11 14:35:56 +0700

    second commit

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const price_1_input = document.querySelector('.price_1');
 const price_2_input = document.querySelector('.price_2');
 const total_price_input = document.querySelector('.total_price');
 const total_price_button = document.querySelector('.total_button');
+const select_pay = document.querySelector('.select_pay');
 console.log(total_price_button);
 total_price_button.addEventListener('click', ()=> {
     tinhToan();
 })
+
+  
 function tinhToan() {
     let price_1 = parseInt(price_1_input.value);
     let price_2 = parseInt(price_2_input.value);
@@ -13,9 +16,17 @@ function tinhToan() {
         alert("Có nhập thôi cũng nhát");
     }
     else {
+        console.log(select_pay.value);
+
+    if (select_pay.value == '0') {
     let c = (price_2-price_1)*10/100 + price_1;
     total_price_input.innerText = convert(c);
-    console.log(c);
+    }
+    if (select_pay.value == '1') {
+    let c = (price_2-price_1)*3/100 + price_1;
+    total_price_input.innerText = convert(c);
+    }
+   
     }
 }
 function convert(number) {
